edca623eb07cbb9a2a3fe62323e19be5d100dad7
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-10-30 18:01:14 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from mycroft import MycroftSkill, intent_file_handler
 from twilio.rest import Client
 
+
 class RemindEvents(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
@@ -14,7 +15,7 @@ class RemindEvents(MycroftSkill):
 
 
 # the following line needs your Twilio Account SID and Auth Token
-client = Client("AC429a4c06f04eb36287f1c2a682c90a2a", "cc764e749192f49c9869c8e16630424a")
+client = Client("AC429a4c06f04eb36287f1c2a682c90a2a", "2d7e4dc79393ccbbd8d4827d076fa24c")
 
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
