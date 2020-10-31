6d6a3673ede780df5b934d05789019dd726f851f
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-10-30 23:27:32 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,12 +15,12 @@ class RemindEvents(MycroftSkill):
 
 
 # the following line needs your Twilio Account SID and Auth Token
-client = Client("AC429a4c06f04eb36287f1c2a682c90a2a", "2d7e4dc79393ccbbd8d4827d076fa24c")
+#client = Client("AC429a4c06f04eb36287f1c2a682c90a2a", "2d7e4dc79393ccbbd8d4827d076fa24c")
 
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
 # account to send SMS to any phone number
-client.messages.create(to="+12012400693", 
+#client.messages.create(to="+12012400693", 
                        from_="+16267095806", 
                        body="Hello from Python!")
 
