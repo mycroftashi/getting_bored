9a317343e0d6652455b447d6ba026dc43f3d97ce
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-01 15:34:07 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+
+import RPi.GPIO as GPIO
+import time
+
+                       
+#start LED
+                       
+                       
+    
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.OUT)
+
+state = True
+
+# endless loop, on/off for 1 second
+while True:
+ GPIO.output(2,True)
+ time.sleep(1)
+ GPIO.output(2,False)
+ time.sleep(1)
+self.speak("Led is working")  
+                       
+# status = GPIO.get("GPIO1")
+ #       self.speak("Led is %s" % status)             
+
+
