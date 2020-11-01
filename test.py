8ddc726877f62928b66fc8a648c6b768634e4f0f
mycroftashi <73615955+mycroftashi@users.noreply.github.com>
2020-11-01 15:45:21 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-
-import RPi.GPIO as GPIO
+import sys
+from os.path import abspath, dirname
+sys.path.append(abspath(dirname(__file__)))
+import GPIO
 import time
                        
 #start LED
